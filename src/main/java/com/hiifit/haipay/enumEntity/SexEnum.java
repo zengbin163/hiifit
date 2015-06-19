b4314d3d6a4da4230e9373fea3628c16c5a8d86e
zengbin163 <532799609@qq.com>
2015-06-19 15:49:16 +0800

    UserAction

---
+++ src/main/java/com/hiifit/haipay/enumEntity/SexEnum.java
@@ -0,0 +1,52 @@
+/*
+ * @Project: GZJK
+ * @Author: bin
+ * @Date: 2015年4月21日
+ * @Copyright: 2000-2015 CMCC . All rights reserved.
+ */
+package com.hiifit.haipay.enumEntity;
+
+/** 
+* @ClassName: DeviceTypeEnum 
+* @Description: 设备类型
+* @author bin
+* @date 2015年4月21日 下午2:31:54 
+*/
+public enum SexEnum {
+    
+    WOMAN(0, "女人"), MAN(1, "男人");
+    
+    private Integer code;
+    
+    private String string;
+    
+    private SexEnum(Integer code, String string) {
+        this.code = code;
+        this.string = string;
+    }
+    
+    public static SexEnum getEnum(Integer code) {
+        for (SexEnum sexEnum : SexEnum.values()) {
+            if (sexEnum.getCode().equals(code)) {
+                return sexEnum;
+            }
+        }
+        return null;
+    }
+    
+    public Integer getCode() {
+        return code;
+    }
+    
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+    
+    public String getString() {
+        return string;
+    }
+    
+    public void setString(String string) {
+        this.string = string;
+    }
+}
--- src/main/java/com/hiifit/haipay/vo/User.java
+++ src/main/java/com/hiifit/haipay/vo/User.java
@@ -1,134 +1,122 @@
 package com.hiifit.haipay.vo;
 
 import java.io.Serializable;
+
+import com.hiifit.haipay.enumEntity.SexEnum;
+
 /**
  * 用户登录表
  * @author sen
  *
  */
-public class User implements Serializable{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6723648415687712333L;
-	/**
-	 * 用户ID
-	 */
-	private Integer id;
-	/**
-	 * 昵称
-	 */
-	private String nickName;
-	
-	/**
-	 * 用户手机号
-	 */
-	private String mobile;
-	/**
-	 * 登录密码
-	 */
-	private String password;
-	/**
-	 * 上次登录时间
-	 */
-	private String lastLoginTime;
+public class User implements Serializable {
+    
+    private static final long serialVersionUID = -6723648415687712333L;
+    
+    /**
+     * 用户ID
+     */
+    private Integer id;
+    
+    /**
+     * 昵称
+     */
+    private String nickName;
+    
+    /**
+     * 用户手机号
+     */
+    private String mobile;
+    
+    /**
+     * 登录密码
+     */
+    private String password;
+    
+    /**
+     * 上次登录时间
+     */
+    private String lastLoginTime;
+    
     /**
      * 是否需要更新敏感词
      */
-	private Integer isUpdate;
-
-	public User() {}
-	
-	public User(String mobile, String password) {
-		this.mobile = mobile;
-		this.password = password;
-	}
-	
-	public User(String mobile, String password,String nickName) {
-        this.mobile = mobile;
-        this.password = password;
-        this.nickName = nickName;
+    private Integer isUpdate;
+    
+    private String headerUrl;
+    
+    private SexEnum sex;
+    
+    public User() {
     }
-	
-	public User(Integer id,String mobile, String password,String nickName,String lastLoginTime) {
-	    this.id = id;
+    
+    public User(String mobile, String password) {
         this.mobile = mobile;
         this.password = password;
-        this.nickName = nickName;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getMobile() {
+        return mobile;
+    }
+    
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getLastLoginTime() {
+        return lastLoginTime;
+    }
+    
+    public void setLastLoginTime(String lastLoginTime) {
         this.lastLoginTime = lastLoginTime;
     }
-	public User(Integer id,String mobile, String password) {
-        this.id = id;
-        this.mobile = mobile;
-        this.password = password;
+    
+    public String getNickName() {
+        return nickName;
     }
-	/**
-	 * @return the id
-	 */
-	public Integer getId() {
-		return id;
-	}
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	/**
-	 * @return the mobile
-	 */
-	public String getMobile() {
-		return mobile;
-	}
-	/**
-	 * @param mobile the mobile to set
-	 */
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
-	/**
-	 * @return the password
-	 */
-	public String getPassword() {
-		return password;
-	}
-	/**
-	 * @param password the password to set
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	/**
-	 * @return the lastLoginTime
-	 */
-	public String getLastLoginTime() {
-		return lastLoginTime;
-	}
-	/**
-	 * @param lastLoginTime the lastLoginTime to set
-	 */
-	public void setLastLoginTime(String lastLoginTime) {
-		this.lastLoginTime = lastLoginTime;
-	}
-	/**
-	 * @return the nickName
-	 */
-	public String getNickName() {
-		return nickName;
-	}
-	/**
-	 * @param nickName the nickName to set
-	 */
-	public void setNickName(String nickName) {
-		this.nickName = nickName;
-	}
-
+    
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+    
     public Integer getIsUpdate() {
         return isUpdate;
     }
-
+    
     public void setIsUpdate(Integer isUpdate) {
         this.isUpdate = isUpdate;
     }
-
+    
+    public String getHeaderUrl() {
+        return headerUrl;
+    }
+    
+    public void setHeaderUrl(String headerUrl) {
+        this.headerUrl = headerUrl;
+    }
+    
+    public SexEnum getSex() {
+        return sex;
+    }
+    
+    public void setSex(SexEnum sex) {
+        this.sex = sex;
+    }
+    
 }
